d7f80380cf8a30a43fddf895deee99e49b20348b
shivam <shiva12dec@gmail.com>
2022-02-13 15:34:18 +0530

    add-school

--- graph_app/schema.py
+++ graph_app/schema.py
@@ -39,10 +39,28 @@ class myQuery(graphene.ObjectType):
 
 
 class myQuery2(graphene.ObjectType):
+    
     all_class = DjangoListField(SClassGraph, school_id=graphene.Int())
+    
     def resolve_all_class(root, info, school_id):
         return SClass.objects.filter(school_id=school_id)
 
 
-my_schema = graphene.Schema(query=myQuery)
+class SchoolMutation(graphene.Mutation):
+    class Arguments:
+        school_name =  graphene.String(required=True)
+    
+    school = graphene.Field(SchoolGraph)
+    @classmethod
+    def mutate(cls, root, info, school_name):
+        school = School(school_name=school_name)
+        school.save()
+        return SchoolMutation(school=school)
+
+class Mutation(graphene.ObjectType):
+    
+    add_school = SchoolMutation.Field()
+
+
+my_schema = graphene.Schema(query=myQuery, mutation=Mutation)
 my_schema2 = graphene.Schema(query=myQuery2)
